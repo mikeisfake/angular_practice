a4ae95775a1110bb306ba0806505f034e53b9850
mikeisfake <mikescooper@icloud.com>
2022-01-22 14:17:05 -0500

    completed counter state and simple fetch request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface PokeTypes {
+  type: {
+    [key: string]: string;
+  }
+}
+export interface Pokemon {
+  name: string;
+  height: number;
+  types: PokeTypes[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'counter_app';
+  constructor(private http: HttpClient) {}
+
+  count = 0
+  pokemon : string = ''
+  currentPokemon : Pokemon[] = []
+
+  increment() {
+    this.count = this.count+=1
+  }
+  decrement() {
+    this.count = this.count-+1
+  }
+
+  handleInput(inputPoke: string) {
+    this.pokemon = inputPoke
+    console.log(this.pokemon)
+  }
+
+  getPokemon() {
+    this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${this.pokemon}/`).subscribe(data => {
+      this.currentPokemon.push(data)
+      console.log(this.currentPokemon[0])
+    });
+  }
 }
